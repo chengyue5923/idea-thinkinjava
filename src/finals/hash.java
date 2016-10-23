7bdaaec2406e51a79f3b8da4f08c621a451c5d97
chengyue <chengyue5923@163.com>
2016-10-23 21:33:01 +0800

    dddd

---
+++ src/finals/hash.java
@@ -0,0 +1,9 @@
+package finals;
+
+/**
+ * Created by lenovo on 2016/10/13.
+ */
+public class hash {
+    public static void main(String args[]){
+    }
+}
--- src/src/Cleanser.java
+++ src/src/Cleanser.java
@@ -4,4 +4,26 @@ package src;
  * Created by lenovo on 2016/10/10.
  */
 public class Cleanser {
+    private String s="Cleanser";
+    public void append(String a){
+        s+=a;
+    }
+    public void diulut(){
+        append("diulut");
+    }
+    public void apply(){
+        append("apply");
+    }
+    public void srcub(){
+        append("srcub");
+    }
+    public String toString(){
+        return s;
+    }
+    public static void main(String args[]){
+        Cleanser cleanser=new Cleanser();
+        cleanser.apply();
+        cleanser.diulut();
+        System.out.println(cleanser);
+    }
 }
